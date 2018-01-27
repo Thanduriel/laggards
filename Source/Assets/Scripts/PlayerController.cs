a09f93ce7991509153e8386421f3c0eacb1ea86b
Thanduriel <rojendersie@alice.de>
2018-01-27 19:26:10 +0100

    + collision # smooth player movement

--- Source/Assets/Scripts/PlayerController.cs
+++ Source/Assets/Scripts/PlayerController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.UI;
 
 public class PlayerController : NetworkBehaviour
 {
@@ -11,22 +10,11 @@ public class PlayerController : NetworkBehaviour
     public Sprite spIdleSprite;
     public Sprite spMoveLeft;
     public Sprite spMoveRight;
-
-    bool _isTransmitting = false;
-    GameObject ExecutionList;
-    // Use this for initialization
-    void Start () {
-        ExecutionList = GameObject.Find("ExecutionList");
-        if(transform.position.x > 0)
-        {
-            transform.GetComponent<SpriteRenderer>().flipX = true;
-        }
-        if (!isLocalPlayer)
-        {
-            SpriteRenderer renderer = transform.GetComponent<SpriteRenderer>();
-            renderer.color = new Color32(184, 255, 88, 255);
-        }
-    }
+	// Use this for initialization
+	void Start () {
+        //Position Player
+        this.transform.position = new Vector3(-4.21f, -2.63f, -2);
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -38,101 +26,50 @@ public class PlayerController : NetworkBehaviour
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
-
-        if(GameManager.GameState == "Transmission" && !_isTransmitting)
-        {
-            _isTransmitting = true;
-            TrasmitActions();
-        }
+		// smooth movement
+		float nextMovement = Mathf.Max(0f, m_bufferedMovement - Time.deltaTime);
+		float move = m_bufferedMovement - nextMovement;
+		m_bufferedMovement = nextMovement;
+       // transform.Rotate(0, x, 0);
+        if(move != 0f) transform.Translate(m_movementDir * move, 0, 0);
     }
-
-    void TrasmitActions()
-    {
-        Debug.Log("Executing Actions");
-
-        //Add Transmissions To List
-        foreach (Transform child in ExecutionList.transform)
-        {
-            if (child.gameObject.GetComponent<Text>().text != "Action Chain:")
-                GameManager.ActionList.Add(child.gameObject.GetComponent<Text>().text);
-        }
-
-        GameManager.GameState = "Transmission";
-
-        StartCoroutine(executeActions(GameManager.ActionList));
-    }
-
-    IEnumerator executeActions(List<string> ActionList)
-    {
-        //Execute Actions
-        foreach (string action in ActionList)
-        {
-            switch (action)
-            {
-                case "MoveRightCard":
-                    MoveRight();
-                    break;
-                case "MoveLeftCard":
-                    MoveLeft();
-                    break;
-                case "JumpCard":
-                    Jump();
-                    yield return new WaitForSeconds(GameManager.executionTime);
-                    Fall();
-                    break;
-                case "EvadeCard":
-                    Evade();
-                    break;
-            }
-            //Delay Actinos 
-            yield return new WaitForSeconds(GameManager.executionTime);
-        }
-
-        GameManager.GameState = "Planning";
-        _isTransmitting = false;
-        ActionList.Clear();
-    }
-
 
     public void MoveLeft()
     {
-        if (!isLocalPlayer) return;
-
-        transform.Translate(new Vector3(-1, 0, 0));
-        gameObject.GetComponent<SpriteRenderer>().sprite = spMoveLeft;
+		m_movementDir = -1f;
+		m_bufferedMovement = 1f;
+	   // transform.Translate(new Vector3(-1, 0, 0));
+		gameObject.GetComponent<SpriteRenderer>().sprite = spMoveLeft;
     }
 
     public void MoveRight()
     {
-        if (!isLocalPlayer) return;
-
-        transform.Translate(new Vector3(1, 0, 0));
+		m_movementDir = 1f;
+		m_bufferedMovement = 1f;
+	//	transform.Translate(new Vector3(1, 0, 0));
         gameObject.GetComponent<SpriteRenderer>().sprite = spMoveRight;
 
     }
 
     public void Jump()
     {
-        if (!isLocalPlayer) return;
-
-        transform.Translate(new Vector3(0, 1, 0));
+		Rigidbody2D body = GetComponent<Rigidbody2D>();
+		body.AddForce(new Vector2(0, 8f), ForceMode2D.Impulse);
+     //   transform.Translate(new Vector3(0, 1, 0));
         gameObject.GetComponent<SpriteRenderer>().sprite = spJumpSprite;
     }
 
     public void Evade()
     {
-        if (!isLocalPlayer) return;
-
         gameObject.GetComponent<SpriteRenderer>().sprite = spEvadeSprite;
     }
 
     public void Fall()
     {
-        if (!isLocalPlayer) return;
-
-        transform.Translate(new Vector3(0, -1, 0));
+      //  transform.Translate(new Vector3(0, -1, 0));
         gameObject.GetComponent<SpriteRenderer>().sprite = spIdleSprite;
     }
+
+	private float m_bufferedMovement;
+	private float m_movementDir;
 }
