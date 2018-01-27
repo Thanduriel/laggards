91f2fe5642fc3c3909c34f8f9c5838c05a2bd103
Tony Schwensfeier <finalcow@yahoo.de>
2018-01-27 15:10:34 +0100

    Add Scene Control

---
+++ Source/Assets/Scripts/GameState.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameState
+{
+    private GameStateController gSC;
+
+    public virtual IEnumerator Init(GameStateController gSC)
+    {
+        this.gSC = gSC;
+        yield break;
+    }
+    public virtual IEnumerator Update()
+    {
+        yield break;
+    }
+}
+
+public class SetupGame : GameState
+{
+    public override IEnumerator Init(GameStateController gSC)
+    {
+        base.Init(gSC);
+        yield break;
+    }
+}
+
+public class ActionSelection : GameState
+{
+
+}
+
+public class PerformAction : GameState
+{
+
+}
+
+public class PauseMenu : GameState
+{
+
+}
