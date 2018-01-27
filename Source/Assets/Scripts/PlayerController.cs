dd8cbd2180fc71a0ff5e65374993d9c777f08ed1
Hudhaifa-PC\Hudhaifa <info@huthaifa-abd.com>
2018-01-27 14:19:20 +0100

    [Hudhaifa] [Full Project] Initial Source

---
+++ Source/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    public Sprite spJumpSprite;
+    public Sprite spEvadeSprite;
+    public Sprite spIdleSprite;
+    public Sprite spMoveLeft;
+    public Sprite spMoveRight;
+	// Use this for initialization
+	void Start () {
+        //Position Player
+        this.transform.position = new Vector3(-4.21f, -2.63f, -2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    public void MoveLeft()
+    {
+        transform.Translate(new Vector3(-1, 0, 0));
+        gameObject.GetComponent<SpriteRenderer>().sprite = spMoveLeft;
+    }
+
+    public void MoveRight()
+    {
+        transform.Translate(new Vector3(1, 0, 0));
+        gameObject.GetComponent<SpriteRenderer>().sprite = spMoveRight;
+
+    }
+
+    public void Jump()
+    {
+        transform.Translate(new Vector3(0, 1, 0));
+        gameObject.GetComponent<SpriteRenderer>().sprite = spJumpSprite;
+    }
+
+    public void Evade()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = spEvadeSprite;
+    }
+
+    public void Fall()
+    {
+        transform.Translate(new Vector3(0, -1, 0));
+        gameObject.GetComponent<SpriteRenderer>().sprite = spIdleSprite;
+    }
+}
---
+++ Source/Assets/Scripts/TransmissionManager.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransmissionManager : MonoBehaviour
+{
+
+    GameObject ExecutionList;
+    GameObject Player;
+    bool _enableTransmission = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        ExecutionList = GameObject.Find("ExecutionList");
+        //Add Button Event
+        Button b = gameObject.GetComponent<Button>();
+        b.onClick.AddListener(delegate () { if (_enableTransmission) { TrasmitActions(); } });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ExecutionList.transform.childCount > 1)
+        {
+            _enableTransmission = true;
+        }
+        else
+        {
+            _enableTransmission = false;
+        }
+
+        if (_enableTransmission)
+        {
+            gameObject.transform.Find("Text").GetComponent<Text>().color = Color.red;
+        }
+        else
+        {
+            gameObject.transform.Find("Text").GetComponent<Text>().color = Color.gray;
+        }
+    }
+
+    void TrasmitActions()
+    {
+        Debug.Log("Transmitting Actions");
+
+        //Create Transmission Array
+        List<string> ActionList = new List<string>();
+
+        //Add Transmissions To List
+        foreach (Transform child in ExecutionList.transform)
+        {
+            if (child.gameObject.GetComponent<Text>().text != "Action Chain:")
+                ActionList.Add(child.gameObject.GetComponent<Text>().text);
+        }
+
+        GameManager.GameState = "Transmission";
+
+        StartCoroutine(executeActions(ActionList));
+
+        
+    }
+
+    IEnumerator executeActions(List<string> ActionList)
+    {
+        //Execute Actions
+        foreach (string action in ActionList)
+        {
+            switch (action)
+            {
+                case "MoveRightCard":
+                    GetPlayer().GetComponent<PlayerController>().MoveRight();
+                    break;
+                case "MoveLeftCard":
+                    GetPlayer().GetComponent<PlayerController>().MoveLeft();
+                    break;
+                case "JumpCard":
+                    GetPlayer().GetComponent<PlayerController>().Jump();
+                    yield return new WaitForSeconds(GameManager.executionTime);
+                    GetPlayer().GetComponent<PlayerController>().Fall();
+                    break;
+                case "EvadeCard":
+                    GetPlayer().GetComponent<PlayerController>().Evade();
+                    break;
+            }
+            //Delay Actinos 
+            yield return new WaitForSeconds(GameManager.executionTime);
+        }
+
+        GameManager.GameState = "Planning";
+    }
+
+    GameObject GetPlayer()
+    {
+        return GameObject.FindGameObjectWithTag("Player");
+    }
+}
