1a5f91aa5185a65a1cbe620deff41740d28097e9
Hudhaifa-PC\Hudhaifa <info@huthaifa-abd.com>
2018-01-28 12:58:55 +0100

    [Hudhaifa] [CreditScene] [Code] Credit

---
+++ Source/Assets/Scripts/CreditSceneTimeout.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditSceneTimeout : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SceneTimeout());
+	}
+
+    private IEnumerator SceneTimeout()
+    {
+        yield return new WaitForSeconds(30);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown("enter"))
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
